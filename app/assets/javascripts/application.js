9dc492fd08db36542d746ca235ca4fbfa4d57d2f
Carlos Fagiani Junior <fagianijunior@gmail.com>
2014-08-31 20:56:50 +0000

    :/

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,7 @@
 // about supported directives.
 //
 //= require bootstrap
-//= require bootstrap-dropdown
 //= require jquery
-//= require jquery-ui
 //= require jquery_ujs
+//= require jquery-ui
 //= require_tree .
